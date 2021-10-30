2663e40a1faa56aefbfbb64c4aa821014cf8bf03
insansantana <insan.santana@gmail.com>
2021-10-30 11:55:09 +0700

    another commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [check, setCheck] = useState({
+    box1: false,
+    box2: false,
+    box3: false,
+  });
+  const [values, setValues] = useState({
+    field1: "",
+    field2: "",
+    field3: "",
+  });
+  const [result, setResult] = useState(0);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setCheck({
+      ...check,
+      [name]: checked,
+    });
+  };
+
+  const handleClickBox = (e) => {
+    const checkboxValue = Object.values(check);
+    const inputValues = Object.values(values);
+    const tmp = [];
+    let result = 0;
+
+    for (let index = 0; index < checkboxValue.length; index++) {
+      if (checkboxValue[index] === true) {
+        tmp.push(inputValues[index]);
+      }
+    }
+
+    if (tmp.length <= 1) {
+      alert("please check minimum 2 box");
+    } else {
+      switch (e.target.value) {
+        case "+":
+          result = tmp.reduce((total, num) => {
+            return parseInt(total) + parseInt(num);
+          });
+          setResult(result);
+          break;
+        case "-":
+          result = tmp.reduce((total, num) => {
+            return parseInt(total) - parseInt(num);
+          });
+          setResult(result);
+          break;
+        case "x":
+          result = tmp.reduce((total, num) => {
+            return parseInt(total) * parseInt(num);
+          });
+          setResult(result);
+          break;
+        case "/":
+          result = tmp.reduce((total, num) => {
+            return parseInt(total) / parseInt(num);
+          });
+          setResult(result);
+          break;
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <input
+          type="text"
+          name="field1"
+          value={values.field1}
+          onChange={handleInputChange}
+        />
+        <input
+          type="checkbox"
+          name="box1"
+          onChange={(event) => handleCheckboxChange(event)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          name="field2"
+          value={values.field2}
+          onChange={handleInputChange}
+        />
+        <input
+          type="checkbox"
+          name="box2"
+          onChange={(event) => handleCheckboxChange(event)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          name="field3"
+          onChange={(event) => handleInputChange(event)}
+        />
+        <input
+          type="checkbox"
+          name="box3"
+          onChange={(event) => handleCheckboxChange(event)}
+        />
+      </div>
+
+      <div>
+        <button
+          style={{ marginRight: "5px" }}
+          onClick={handleClickBox}
+          value="+"
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </button>
+        <button
+          style={{ marginRight: "5px" }}
+          onClick={handleClickBox}
+          value="-"
+        >
+          -
+        </button>
+        <button
+          style={{ marginRight: "5px" }}
+          onClick={handleClickBox}
+          value="x"
+        >
+          x
+        </button>
+        <button
+          style={{ marginRight: "5px" }}
+          onClick={handleClickBox}
+          value="/"
+        >
+          /
+        </button>
+      </div>
+      <hr />
+      <div>
+        <span>Hasil :</span> {result}
+      </div>
     </div>
   );
 }
